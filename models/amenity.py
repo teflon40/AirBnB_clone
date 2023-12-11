798cad4c19ad7451de04f497d2731bdd0f0dcd54
teflon40 <frimpong.bernard3220@gmail.com>
2023-12-11 04:10:03 +0100

    Module for class Amenity

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a class Amenity which models real life amenities
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Simulates amenities in a city"""
+    name = ""
