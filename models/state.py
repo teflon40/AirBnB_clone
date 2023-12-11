60b11e5645e4c968fe0a220fabf93872d141da77
teflon40 <frimpong.bernard3220@gmail.com>
2023-12-11 04:11:59 +0100

    Module for state

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a class State which models real life state
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Simulates states"""
+    name = ""
