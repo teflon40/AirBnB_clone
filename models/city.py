c1d241237ea7240e949533c72fa7b9b409a02d47
teflon40 <frimpong.bernard3220@gmail.com>
2023-12-11 04:09:22 +0100

    module for class City

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a class city which models real life cities
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Simulates cities"""
+    name = ""
+    state_id = ""
